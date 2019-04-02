3e6b2392bfddcb7eab9955eaa408d5d7271dbaa9
MeiK <meik2333@gmail.com>
2019-04-02 09:19:55 +0800

    Update

---
+++ judgelight_go.go
@@ -0,0 +1,23 @@
+package JudgeLight_Go
+
+func Run(
+	execFilePath string,
+	execArgs []string,
+	envs []string,
+	cpuTimeLimit int,
+	readTimeLimit int,
+	memoryLimit int,
+	outputSizeLimit int,
+	stackLimit int,
+
+	inputFilePath string,
+	outputFilePath string,
+	errorFilePath string,
+
+	uid string,
+	gid string,
+	syscallRule []bool,
+) Result {
+	result := Result{}
+	return result
+}
---
+++ result.go
@@ -0,0 +1,10 @@
+package JudgeLight_Go
+
+type Result struct {
+	cpuTimeUsed int
+	realTimeUsed int
+	memoryUsed int
+	signal int
+	reFlag bool
+	reSyscall int
+}
