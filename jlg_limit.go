f205aa4412fd2d9aeaadda10749d898c9487f9e1
MeiK <meik2333@gmail.com>
2019-04-03 10:28:36 +0800

    添加资源限制与流重定向等

--- jlg_limit.go
+++ jlg_limit.go
@@ -1,7 +1,6 @@
 package JudgeLight_Go
 
 import (
-	"errors"
 	"golang.org/x/sys/unix"
 )
 
@@ -13,14 +12,102 @@ func SetProcLimit(
 ) error {
 	var rl = unix.Rlimit{}
 
-	// cpu time limit
+	// cpu time limit (MS)
 	if cpuTimeLimit != 0 {
 		rl.Cur = uint64(cpuTimeLimit/1000 + 1)
 		rl.Max = rl.Cur
 		if err := unix.Setrlimit(unix.RLIMIT_CPU, &rl); err != nil {
-			return errors.New("set cpu limit failure")
+			return err
 		}
 	}
 
+	// memory limit (KiB)
+	// see: https://ux.stackexchange.com/questions/13815/files-size-units-kib-vs-kb-vs-kb
+	if memoryLimit != 0 {
+		rl.Cur = uint64(memoryLimit * 1024)
+		rl.Max = rl.Cur
+		if err := unix.Setrlimit(unix.RLIMIT_DATA, &rl); err != nil {
+			return err
+		}
+
+		rl.Cur = rl.Cur * 2
+		rl.Max = rl.Cur
+		if err := unix.Setrlimit(unix.RLIMIT_AS, &rl); err != nil {
+			return err
+		}
+	}
+
+	// stack limit (KiB)
+	if stackLimit != 0 {
+		rl.Cur = uint64(stackLimit * 1024)
+		rl.Max = rl.Cur
+		if err := unix.Setrlimit(unix.RLIMIT_STACK, &rl); err != nil {
+			return err
+		}
+	}
+
+	// output size limit (B)
+	if outputSizeLimit != 0 {
+		rl.Cur = uint64(outputSizeLimit)
+		rl.Max = rl.Cur
+		if err := unix.Setrlimit(unix.RLIMIT_FSIZE, &rl); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func SetProcUser(uid int, gid int) error {
+	// set uid
+	if uid != 0 {
+		if err := unix.Setuid(uid); err != nil {
+			return err
+		}
+	}
+
+	// set gid
+	if gid != 0 {
+		if err := unix.Setgid(gid); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func SetProcStream(inputFilePath string, outputFilePath string, errorFilePath string) error {
+	// set input stream
+	if inputFilePath != "" {
+		// open file
+		if fd, err := unix.Open(inputFilePath, unix.O_RDONLY, 666); err != nil {
+			return err
+		} else {
+			// dup pipe
+			if err := unix.Dup2(fd, unix.Stdin); err != nil {
+				return err
+			}
+		}
+	}
+
+	// set output stream
+	if outputFilePath != "" {
+		if fd, err := unix.Open(outputFilePath, unix.O_WRONLY, 666); err != nil {
+			return err
+		} else {
+			if err := unix.Dup2(fd, unix.Stdout); err != nil {
+				return err
+			}
+		}
+	}
+
+	// set error stream
+	if errorFilePath != "" {
+		if fd, err := unix.Open(errorFilePath, unix.O_WRONLY, 666); err != nil {
+			return err
+		} else {
+			if err := unix.Dup2(fd, unix.Stderr); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
--- judgelight_go.go
+++ judgelight_go.go
@@ -30,13 +30,41 @@ func Run(
 
 	var ForkLock sync.RWMutex
 	ForkLock.Lock()
+
 	pid, _, _ := unix.Syscall(unix.SYS_FORK, 0, 0, 0)
 
 	if pid < 0 { // fork error
 		return result, errors.New("fork failure")
 	} else if pid == 0 { // child
+		// set limit
+		if err := SetProcLimit(cpuTimeLimit, memoryLimit, stackLimit, outputSizeLimit); err != nil {
+			panic(err)
+		}
 
-		// run
+		// set user
+		if err := SetProcUser(uid, gid); err != nil {
+			panic(err)
+		}
+
+		// chroot
+		// before SetProcStream
+		if chroot != "" {
+			if err := unix.Chroot(chroot); err != nil {
+				panic(err)
+			}
+		}
+
+		// set stream
+		if err := SetProcStream(inputFilePath, outputFilePath, errorFilePath); err != nil {
+			panic(err)
+		}
+
+		// set ptrace
+		if _, _, err := unix.Syscall(unix.SYS_PTRACE, uintptr(unix.PTRACE_TRACEME), 0, 0); err != 0 {
+			panic(err)
+		}
+
+		// exec run
 		if err := unix.Exec(execFilePath, append([]string{execFilePath}, execArgs...), envs); err != nil {
 			panic(err)
 		}
