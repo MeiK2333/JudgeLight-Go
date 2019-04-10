32a25d256a36be94288dff6d1e4086cf2c71a89b
MeiK <meik2333@gmail.com>
2019-04-10 10:21:47 +0800

    add test

--- limit_test.go
+++ limit_test.go
@@ -5,6 +5,33 @@ import (
 	"testing"
 )
 
+func TestCpuTimeLimit(t *testing.T) {
+	result, err := Run(
+		"/usr/bin/python3",
+		[]string{"tests/loopcpu.py"},
+		[]string{},
+		1000,
+		5000,
+		65535,
+		65535,
+		655350,
+		"",
+		"",
+		"",
+		0,
+		0,
+		"",
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("test cpu time limit:", result)
+	if result.cpuTimeUsed < 1000 || result.cpuTimeUsed >= 3000 {
+		t.Fail()
+	}
+}
+
 func TestRealTimeLimit(t *testing.T) {
 	result, err := Run(
 		"/bin/sleep",
@@ -54,4 +81,7 @@ func TestOutputSizeLimit(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println("test output size limit:", result)
+	if !result.reFlag {
+		t.Fail()
+	}
 }
---
+++ tests/loopcpu.py
@@ -0,0 +1,4 @@
+ans = 0
+for i in range(2 ** 32):
+    ans += i
+print(ans)
