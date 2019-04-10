dfa604ba98a75fe109c59ce97865a5d8e5520fad
MeiK <meik2333@gmail.com>
2019-04-10 13:59:03 +0800

    add Result String()

--- result.go
+++ result.go
@@ -1,5 +1,7 @@
 package JudgeLight_Go
 
+import "fmt"
+
 type Result struct {
 	cpuTimeUsed  int
 	realTimeUsed int
@@ -7,4 +9,13 @@ type Result struct {
 	signal       int
 	reFlag       bool
 	reSyscall    int
+}
+
+func (r Result) String() string {
+	return fmt.Sprintf("\nCPU time used:\t%d\n"+
+		"real time used:\t%d\n"+
+		"memory used:\t%d\n"+
+		"signal:\t\t%d\n"+
+		"reFlag:\t\t%t\n"+
+		"reSyscall:\t%d\n", r.cpuTimeUsed, r.realTimeUsed, r.memoryUsed, r.signal, r.reFlag, r.reSyscall)
 }
